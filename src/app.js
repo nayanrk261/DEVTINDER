be0a8d93277fac9c60fd2c29d3df79e59ca43c2b
NYN KHANDELWAL <your_email@example.com>
2026-05-21 11:19:38 +0530

    ep4

--- src/app.js
+++ src/app.js
@@ -2,22 +2,51 @@ const express = require("express");
 
 const app = express();
 
+//optional b
+app.get(/^\/a(b)?c$/,(req,res) => {
+    res.send("get request")
+})
+
+//one or more b
+app.get(/^\/ab+c$/, (req,res)=>{
+    res.send("matched +");
+});
+
+//zero or more b
+app.get(/^\/ab*c$/, (req,res)=>{
+    res.send("matched *");
+});
+
 app.get("/user",(req,res) => {
-    res.send("User get request")
+    console.log(req.query)
+    res.send({ firstname: "Nayan", lastname : "Khandelwal"})
 })
 
-app.post("/user",(req,res) => {
-    res.send("User post request")
+app.get("/user/:userId/:name",(req,res) => {
+    console.log(req.params)
+    res.send({ firstname: "Nayan", lastname : "Khandelwal"})
 })
 
-app.delete("/user",(req,res) => {
-    res.send("User delete request")
-})
 
-app.patch("/user",(req,res) => {
-    res.send("User patch request")
-})
+
+// app.get("/user",(req,res) => {
+//     res.send("User get request")
+// })
+
+// app.post("/user",(req,res) => {
+//     res.send("User post request")
+// })
+
+// app.delete("/user",(req,res) => {
+//     res.send("User delete request")
+// })
+
+// app.patch("/user",(req,res) => {
+//     res.send("User patch request")
+// })
 
 app.listen(3333, () => {
     console.log("server started at port 3333")
 })
+
+//?,+,*,regex,query,dynamic routes,params
